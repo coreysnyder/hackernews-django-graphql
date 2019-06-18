859b7f59e56e7a0e187f6471f05f003c22d68628
Corey Snyder <corey.snyder@aver.io>
2019-06-18 17:03:18 -0400

    Adds Vote type for adding votes to Links

--- links/schema.py
+++ links/schema.py
@@ -2,17 +2,19 @@ import graphene
 from graphene_django import DjangoObjectType
 from users.schema import UserType
 
-from .models import Link
+
+#from .models import Link, Vote
+from links.models import Link, Vote
 
 class LinkType(DjangoObjectType):
     class Meta:
         model = Link
 
-class Query(graphene.ObjectType):
-    links = graphene.List(LinkType)
 
-    def resolve_links(self, info, **kwargs):
-        return Link.objects.all()
+class VoteType(DjangoObjectType):
+    class Meta:
+        model = Vote
+
 
 class CreateLink(graphene.Mutation):
     id = graphene.Int()
@@ -41,6 +43,39 @@ class CreateLink(graphene.Mutation):
             posted_by=link.posted_by,
         )
 
+class CreateVote(graphene.Mutation):
+    user = graphene.Field(UserType)
+    link = graphene.Field(LinkType)
+
+    class Arguments:
+        link_id = graphene.Int()
+
+    def mutate(self, info, link_id):
+        user = info.context.user
+        if user.is_anonymous:
+            raise Exception('You must be logged to vote!')
+
+        link = Link.objects.filter(id=link_id).first()
+        if not link:
+            raise Exception('Invalid Link!')
+
+        Vote.objects.create(
+            user=user,
+            link=link,
+        )
+
+        return CreateVote(user=user, link=link)
+
+class Query(graphene.ObjectType):
+    links = graphene.List(LinkType)
+    votes = graphene.List(VoteType)
+
+    def resolve_links(self, info, **kwargs):
+        return Link.objects.all()
+
+    def resolve_votes(self, info, **kwargs):
+        return Vote.objects.all()
 
 class Mutation(graphene.ObjectType):
     create_link = CreateLink.Field()
+    create_vote = CreateVote.Field()
