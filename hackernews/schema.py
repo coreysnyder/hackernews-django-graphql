8d0bbbe66f170254528593ff4b4472a740b42314
Corey Snyder <corey.snyder@aver.io>
2019-06-17 21:36:02 -0400

    Adds mutation and query for users, next up Authentication!

--- hackernews/schema.py
+++ hackernews/schema.py
@@ -1,11 +1,14 @@
 import graphene
 
 import links.schema
+import users.schema
 
-class Query(links.schema.Query, graphene.ObjectType):
+class Query(links.schema.Query, users.schema.Query, graphene.ObjectType):
     pass
 
-class Mutation(links.schema.Mutation, graphene.ObjectType):
+
+class Mutation(
+    users.schema.Mutation, links.schema.Mutation, graphene.ObjectType,):
     pass
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
